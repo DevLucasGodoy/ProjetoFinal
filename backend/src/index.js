6af838be4bc7644f56bd27e20ad7825ee657c57d
Lucas Godoy <54687280+DevLucasGodoy@users.noreply.github.com>
2022-12-02 14:07:47 -0400

    Delete index.js

--- backend/src/index.js
+++
@@ -1,7 +0,0 @@
-const { response } = require('express');
-const express = require('express');
-const app = express();
-
-app.get('/', (request, response) => response.status(200).send('API Projeto Final'));
-
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
