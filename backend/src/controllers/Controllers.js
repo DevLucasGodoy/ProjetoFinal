dfb5c47bb7e3726d8be577415f758624db0b5834
Lucas Godoy <54687280+DevLucasGodoy@users.noreply.github.com>
2022-12-02 19:52:22 -0400

    Update Controllers.js

--- backend/src/controllers/Controllers.js
+++ backend/src/controllers/Controllers.js
@@ -3,7 +3,6 @@ const Model = require('../models/Model');
 
 const getAll = async (require, response) => {
     const Models = await Model.getAll();
-
     return response.status(200).json(Models);
 };
 
