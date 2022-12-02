b87c439e8bb190d0d57ed4be3885503026deb128
Lucas Godoy <54687280+DevLucasGodoy@users.noreply.github.com>
2022-12-02 15:21:17 -0400

    Update router.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const Controller = require('./controllers/Controllers');
 
 router.get('/', (req, res) => res.status(200).send('API Projeto Final'));
-
+router.get('/produtos', Controller.getAll);
 
 module.exports = router;
