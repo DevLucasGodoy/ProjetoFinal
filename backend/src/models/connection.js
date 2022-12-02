bb2f670e97acf6bfa4c8d52331df8d409ac61711
Lucas Godoy <54687280+DevLucasGodoy@users.noreply.github.com>
2022-12-02 19:38:01 -0400

    Update connection.js

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -1,13 +1,18 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-console.log(process.env.MYSQL_HOST);
-
 const connection = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'projeto_final'
 });
+
+// const connection = mysql.createPool({
+//     host: process.env.MYSQL_HOST,
+//     user: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_PASSWORD,
+//     database: process.env.MYSQL_DATABASE,
+// });
 
 module.exports = connection;
