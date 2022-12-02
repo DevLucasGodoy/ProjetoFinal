b2d906bc78ecdc2cfb1a770cb93b9712d509ca4e
Lucas Godoy <54687280+DevLucasGodoy@users.noreply.github.com>
2022-12-02 16:21:59 -0400

    Update Controllers.js

--- backend/src/controllers/Controllers.js
+++ backend/src/controllers/Controllers.js
@@ -1,7 +1,10 @@
 const { response } = require("express");
+const Model = require('../models/Model');
 
-const getAll = (require, response) => {
-    return response.status(200).json({message: 'Controller em funcionamento!'});
+const getAll = async (require, response) => {
+    const Models = await Model.getAll();
+
+    return response.status(200).json(Models, {message: 'Controller em funcionamento!'});
 };
 
 module.exports ={
