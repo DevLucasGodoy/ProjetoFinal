a89ba71a5d023d91bba4c600dd1f62b106d3da38
Lucas Godoy <54687280+DevLucasGodoy@users.noreply.github.com>
2022-12-02 15:21:20 -0400

    Update Model.js

--- backend/src/models/Model.js
+++ backend/src/models/Model.js
@@ -1,5 +1,8 @@
-const getAll = () => {
+const connection = require('./connection');
 
+const getAll = async() => {
+    const produtos = await connection.execute('SELECT * FROM produtos');
+    return produtos;
 };
 
 module.exports = {
